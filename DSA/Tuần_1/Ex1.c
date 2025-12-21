feea8d45652e0efacc7245d76e0e668f60aeb52b
Do Quang Phong <wangphong1211@gmail.com>
2025-12-21 12:52:26 +0700

    Refactor Ex1.c to use static array and value
    Updated the program to use a predefined array and a fixed value for searching. Removed user input for array size and elements.

--- DSA/Tuần_1/Ex1.c
+++ DSA/Tuần_1/Ex1.c
@@ -1,17 +1,7 @@
-//Cho một dãy số thực có n phần tử và một số nguyên m. Tìm số có giá trị gần nhất với số m đã cho trong dãy.
 #include <stdio.h>
-#define MAX 100
+#include <math.h>
 
-void nhapMang(float a[], int *n){
-    printf("Nhap so phan tu cua mang: \n");
-    scanf("%d", n);
-    printf("So phan tu cua mang la: %d\n", *n);
-    printf("Nhap cac phan tu cua mang: \n");
-    for(int i = 0; i < *n; i++){
-        printf("Nhap a[%d]: ", i);
-        scanf_s("%f", &a[i]);
-    }
-}
+#define MAX 100
 
 void xuatMang(float a[], int n){
     printf("Cac phan tu cua mang la:\n");
@@ -19,8 +9,8 @@ void xuatMang(float a[], int n){
         printf("%.2f ", a[i]);
     }
 }
-int searchClosest(float a[], int n, int value){
-    int closestIndex = -1;
+int searchClosest(float a[], int n, float value){
+    int closestIndex = 0;
     float minDiff = fabs(a[0] - (float)value);
     for(int i = 1; i < n; i++){
         float diff = fabs(a[i] - (float)value);
@@ -32,18 +22,20 @@ int searchClosest(float a[], int n, int value){
     return closestIndex;
 }
 int main(){
-    float a[MAX] ;
-    int n;
-    printf("Bai tap 1, Chuong 1, Tu tuan 1\n");
-    nhapMang(a, &n);
-    int value;
-    printf("Nhap gia tri:");
+    float a[] = {1, 2, -4, 8, 9, 10, 4, 5, -2, 7};
+    int n = 10;
+    float value = 9.7;
     scanf("%d", &value);
     int index = searchClosest(a, n, value);
     if(index != -1){
-        printf("So gan nhat voi %d trong mang la: %.2f\n", value, a[index]);
+        printf("So gan nhat voi %.2f trong mang la: %.2f\n", value, a[index]);
     } else {
         printf("Khong the tim thay.\n");
     }
     return 0;
 }
+
+/*
+Output:
+So gan nhat voi 9.70 trong mang la: 10.00
+*/
