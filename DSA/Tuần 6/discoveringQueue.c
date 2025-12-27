9bd3123ebad4ddef609725690c0509a0e2718578
Do Quang Phong <wangphong1211@gmail.com>
2025-12-27 09:48:31 +0700

    Refactor queue and stack implementations

--- DSA/Tuần 6/discoveringQueue.c
+++ DSA/Tuần 6/discoveringQueue.c
@@ -1,76 +1,132 @@
 #include <stdio.h>
-#define MAX 5
 
-typedef struct {
-    int head, tail;
-    int a[MAX];
-} Queue;
+#define MAX 100
 
-void init(Queue *q) {
-    q -> head = 0;
-    q -> tail = -1;
+typedef struct stack{
+    int data[MAX];
+    int top;
+}stack;
+
+typedef struct queue{
+  int data[MAX];
+  int head, tail;
 }
 
-int isFull(Queue *q){
-    if(q -> tail == MAX -1){
+//Some function to preprocessing stack
+void initStack(stack *s){
+    s -> top = -1;
+}
+
+int isFull(stack s){
+    if(s.top == MAX-1){
         return 1;
     }
     return 0;
 }
 
-int isEmpty(Queue *q){
-    if(q -> tail < q -> head){
+int isEmpty(stack s){
+    if(s.top == -1){
         return 1;
     }
     return 0;
 }
 
-void put(Queue *q, int val){
-    q -> tail++;
-    (*q).a[(*q).tail] = val;
+int pop(stack *s){
+  if(!isEmpty(*s)) return s -> data[(s->top)--]; 
+  return -1;
 }
 
-int get(Queue* q){
-    int val = (*q).a[q -> head];
-    q -> head++;
-    return val;
+int top(stack *s){
+  if(!isEmpty(*s)){
+    return s->data[(s->top)];
+  }
+  else{
+    return -1;
+  }
 }
 
-void displayQueue(Queue*q){
-    for(int i = q -> head; i <= q -> tail; i++){
-        printf("%d ", (*q).a[i]);
-    }
+void push(stack *s, int val){
+  if(!isFull(*s)){
+    s->data[++(s->top)] = val;
+  }
+  return;
 }
 
-int main() {
-    Queue q;
-    init(&q);
-    int num[6] = {1,-2,3,-4,5,-6};
-    // push
-   printf("     < MAX = %d >   \n\n", MAX);
-    printf("======== PUT ========\n");
-    for (int i = 0; i < 6; i++){
-        printf("put %d: \n",num[i]);
-        if(!isFull(&q)){
-            put(&q,num[i]);
-            displayQueue(&q);
-            printf("\n");
-        }
-        else
-            printf("Queue is full! \n\n");
-    }
-    // get
-    printf("======== GET ========\n");
-    int i = 0;
-    while (!isEmpty(&q)) {
-        printf("get %d: %d\n",i, get(&q));
-        displayQueue(&q);
-        i++;
-        printf("\n");
-     }
-    return 0;
+//Some function to preprocessing queue
+void initQueue(queue *q){
+  q->head = 0;
+  q->tail = -1;
 }
 
+int isEmpty(queue q){
+  if(q.tail == -1){
+    return 1;
+  }
+  return 0;
+}
 
+int isFull(queue q){
+  if(q.head == MAX-1){
+    return 1;
+  }
+  return 0;
+}
 
- 
+//Print array from i = 0 -> i = n-1 with recursion
+void printArrUsingRecursion(int arr[], int i){
+  if(i == 0){
+    return;
+  }
+  printArrUsingRecursion(arr, i - 1);
+  printf("a[%d] = %d\n", i - 1, arr[i-1]);
+}
+
+//Find an elements in the array by scanning the array from i = n until meet the value else return -1;
+int findElementWithRecursion(int arr[], int n, int value){
+  if(arr[n] == value){
+    return n;
+  }
+  
+  if( n < 0){
+    return -1;
+  }
+  
+  findElementWithRecursion(arr, --n, value);
+}
+
+//popping stack with recursion
+int  popWithRecursion(stack *s, int n){
+  //base case
+  if(isEmpty(*s)){
+    return;
+  }
+  printf("%d \n", pop(s));
+  popWithRecursion(s, --n);
+}
+
+//
+int main(){
+  int arr[] = {1, 0, 0, 9, 0, 6};
+  printf("Printing arr using recursion:\n");
+  printArrUsingRecursion(arr, 6);
+  printf("================\n");
+  
+  int el = findElementWithRecursion(arr, 6, 0);
+  printf("Found 0 at %d\n", el);
+  printf("================\n");
+  
+  stack s;
+  initStack(&s);
+  int n = 20;
+  for(int i = 0; i < 20; i ++){
+    push(&s,i);
+  }
+  printf("Popping array with recursion:\n");
+  popWithRecursion(&s, 19);
+  printf("================\n");
+  
+  
+  
+  
+  return 0;
+}
