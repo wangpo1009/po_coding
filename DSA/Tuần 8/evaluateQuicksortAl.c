538e94bd0094929a4962919d13e6192a46e643b0
Do Quang Phong <wangphong1211@gmail.com>
2026-01-03 00:45:23 +0700

    Refactor quicksort partition function and tests

--- DSA/Tuần 8/evaluateQuicksortAl.c
+++ DSA/Tuần 8/evaluateQuicksortAl.c
@@ -20,31 +20,23 @@ float mean(int arr[], int n, int k){
 }
 
 int partition(int arr[], int left, int right){
-  int pivot = right;
-  right--;
-  
-  while(left <= right && left <= pivot){
-    while(arr[pivot] < arr[right]){
-      right--;
+    int pivot = arr[right];
+    int i = left - 1;
+
+    for(int j = left; j < right; j++){
+        if(arr[j] <= pivot){
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
     }
-    
-    while(arr[pivot] > arr[left]){
-      left++;
-    }
-    
-    if(left < right){
-      int temp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = temp;
-    }
-    
-  }
-  
-  int temp = arr[left];
-  arr[left] = arr[pivot];
-  arr[pivot] = temp;
-  
-  return left;
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[right];
+    arr[right] = temp;
+
+    return i + 1;
 }
 
 void quickSort(int arr[], int left, int right, int* count){
@@ -64,13 +56,13 @@ int main(){
   int k[4] = {10, 100, 1000, 10000};
   int n = 10;
   int count = 0;
-  int freqTab[100] = {};
+  int freqTab[10] = {};
   int arr[MAX];
   
   
   for(int i = 0; i < 4; i++){
     
-    for(int j = 0; j < 100; j++){
+    for(int j = 0; j < 10; j++){
       freqTab[j] = 0;
     } 
     
@@ -83,17 +75,70 @@ int main(){
     
     printf("WITH K = %d: \n",k[i]);
     printf("Count: ");
-    for(int j = 0; j < 100; j++){
+    for(int j = 0; j < 10; j++){
       printf(" %d \n", freqTab[j]);
     }
-    float mn = mean(freqTab, 100, k[i]);
-    printf("Mean : %.4f", mn);
+    float mn = mean(freqTab, 10, k[i]);
+    printf("Mean : %.4f\n", mn);
     printf("===========================\n");
   }
   
   
   return 0;
 }
+/*
+WITH K = 10: 
+Count:  0 
+ 0 
+ 0 
+ 0 
+ 0 
+ 0 
+ 8 
+ 2 
+ 0 
+ 0 
+Mean : 6.2000
+===========================
+WITH K = 100: 
+Count:  0 
+ 0 
+ 0 
+ 0 
+ 0 
+ 4 
+ 50 
+ 42 
+ 4 
+ 0 
+Mean : 6.4600
+===========================
+WITH K = 1000: 
+Count:  0 
+ 0 
+ 0 
+ 0 
+ 0 
+ 95 
+ 511 
+ 353 
+ 41 
+ 0 
+Mean : 6.3400
+===========================
+WITH K = 10000: 
+Count:  0 
+ 0 
+ 0 
+ 0 
+ 0 
+ 978 
+ 5090 
+ 3575 
+ 357 
+ 0 
+Mean : 6.3311
+===========================*/
 
 
 
