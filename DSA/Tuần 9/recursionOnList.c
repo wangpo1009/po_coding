bc998c6697ba4d24f060a2b08b8f79f66326d85e
Do Quang Phong <wangphong1211@gmail.com>
2026-01-10 17:40:26 +0700

    Refactor linked list structure and functions

--- DSA/Tuần 9/recursionOnList.c
+++ DSA/Tuần 9/recursionOnList.c
@@ -4,28 +4,28 @@
 
 #define MAX 100
 
-typedef struct node{
+typedef struct Node{
   int data;
-  struct node* next;
-}next;
+  struct Node* next;
+}Node;
 
-typedef struct linkedList{
-  node* head;
-}linkedList;
+typedef struct LinkedList{
+  Node* head;
+}LinkedList;
 
 
-void initList(linkedList* list){
+void initList(LinkedList* list){
   list->head = NULL;
 }
 
-void freeList(linkedList* list){
+void freeList(LinkedList* list){
   if(list->head == NULL){
     return ;
   }
   
-  node* current = list->head;
+  Node* current = list->head;
   while(current != NULL){
-    node* temp = current->next;
+    Node* temp = current->next;
     free(current);
     current = temp;
   }
@@ -33,39 +33,121 @@ void freeList(linkedList* list){
   list->head = NULL;
 }
 
-node* createNode(int val){
-  node* newNode = (node*)malloc(sizeof(node));
+Node* createNode(int val){
+  Node* newNode = (Node*)malloc(sizeof(Node));
   newNode->data = val;
   newNode->next = NULL;
   return newNode;
 }
 
 //insert linked list using recursion
-void insertAtPosition(linkedList* list, node* current, int pos, int val, int n){
+void insertAtPosition(LinkedList* list, Node* current, int pos, int val){
     if(current == NULL){
         printf("Invalid position!\n");
         return;
     }
-
-    if(n == pos - 1){
-        node* newNode = createNode(val);
+    
+    Node* newNode = createNode(val);
+    if(pos == 0){
+        newNode->next = list->head;
+        list->head = newNode;
+        return;
+    }
+    
+    if(pos - 1 == 0){
         newNode->next = current->next;
         current->next = newNode;
         return;
     }
 
-    insertAtPosition(list, current->next, pos, val, n + 1);
+    insertAtPosition(list, current->next, pos-1, val);
+}
+
+void insertEndList(LinkedList* list, int inputVal){
+  
+  if(list->head == NULL){
+    list->head = createNode(inputVal);
+    return;
+  }
+  
+  Node* current = list->head;
+  while( current->next != NULL ){
+    current = current->next;
+  }
+  current->next = createNode(inputVal);
+  
 }
 
 //delete node using recursion
-void deleteAtPosition(linkedList* list, node* current, int pos, int val, int n){
-  if(current == NULL){
-    
+void deleteAtPosition(LinkedList* list, Node* current, int pos){
+  if(list->head == NULL){
+    printf("Invalid Position!\n");
+    return;
   }
+  
+  if( pos == 0 ){
+    Node* temp = list->head;
+    list->head = temp->next;
+    free(temp);
+    return;
+  }
+  
+  if(current == NULL || current->next == NULL){
+    printf("Invalid position!\n");
+    return;
+  }
+
+  if( pos == 1 ){
+    Node* temp = current->next;
+    current->next = temp->next;
+    free(temp);
+    return;
+  }
+  
+  deleteAtPosition(list, current->next, pos-1);
+  
+}
+
+void printList(LinkedList* list){
+  if(list->head == NULL){
+    printf("List is empty!\n");
+    return;
+  }
+  
+  printf("List: ");
+  Node* current = list->head;
+  while(current != NULL){
+    printf("%d -> ", current->data);
+    current = current->next;
+  }
+  printf("NULL\n");
 }
 
 
 int main(){
+  LinkedList list;
+  initList(&list);
   
+  for(int i = 9; i >= 0; i--){
+    insertEndList(&list, i);
+  }
+  printList(&list);
+  
+  insertAtPosition(&list, list.head, 0, 10);
+  printList(&list);
+  
+  deleteAtPosition(&list, list.head, 10);
+  printList(&list);
   return 0;
 }
+
+/*
+List: 9 -> 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+List: 10 -> 9 -> 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+List: 10 -> 9 -> 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+*/
+
+
+
+
+
