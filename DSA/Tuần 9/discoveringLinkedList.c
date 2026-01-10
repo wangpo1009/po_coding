6dcbcb630adad9c47a06b59825372a070c911965
Do Quang Phong <wangphong1211@gmail.com>
2026-01-10 16:52:03 +0700

    Refactor linked list implementation with new struct

--- DSA/Tuần 9/discoveringLinkedList.c
+++ DSA/Tuần 9/discoveringLinkedList.c
@@ -1,137 +1,168 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
+
+typedef struct Node{
   int data;
-  struct node* next;
-}node;
+  struct Node* next;
+}Node;
 
-typedef struct linkedList{
-  node* head;
-}linkedList;
+typedef struct LinkedList{
+  Node* head;
+}LinkedList;
 
-void initLinkedList(linkedList* list){
+void initList(LinkedList* list){
   list->head = NULL;
 }
 
-node* createNode(int val){
-  node* newNode = (node*)malloc(sizeof(node));
-  newNode->data = val;
-  newNode->next = NULL;
-  return newNode;
-}
-
-void freeList(linkedList* list){
-  node* current = list->head;
-  node* nextNode = NULL;
+void freeList(LinkedList* list){
+  if(list->head == NULL){
+    return;
+  }
   
+  Node* current = list->head;
   while(current != NULL){
-    nextNode = current->next;
+    Node* temp = current->next;
     free(current);
-    current = nextNode;
+    current = temp;
   }
   list->head = NULL;
 }
 
-void printList(linkedList* list){
-  node* current = list->head;
+
+Node* createNode(int inVal){
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->data = inVal;
+  newNode->next = NULL;
+  return newNode;
+}
+
+void printList(LinkedList* list){
+  if(list->head == NULL){
+    printf("List is empty!\n");
+    return;
+  }
   
+  printf("List: ");
+  Node* current = list->head;
   while(current != NULL){
-    printf("%d -> ",current->data);
+    printf("%d -> ", current->data);
     current = current->next;
   }
   printf("NULL\n");
 }
 
-//problem: insert node as the result linked list is in ascending order
-//assume the given list is already in ascending order
-void insertAscending(linkedList* list, int val){
-  node* newNode = createNode(val);
+//========================== INSERT ==============================
+void insertEndList(LinkedList* list, int inputVal){
   
-  if(list->head == NULL || list->head->data >= val){
-    newNode->next = list->head;
+  if(list->head == NULL){
+    list->head = createNode(inputVal);
+    return;
+  }
+  
+  Node* current = list->head;
+  while( current->next != NULL ){
+    current = current->next;
+  }
+  current->next = createNode(inputVal);
+  
+}
+
+void insertHeadList(LinkedList* list, int inputVal){
+  Node* newNode = createNode(inputVal);
+  
+  if(list->head == NULL){
+    list->head = newNode;
+  }
+  
+  newNode->next = list->head;
+  list->head = newNode;
+}
+
+//Assume that all the input val is valid
+void insertAtPos(LinkedList* list, int pos, int inputVal){
+  Node* newNode = createNode(inputVal);
+  
+  if(list->head == NULL){
     list->head = newNode;
     return;
   }
   
-  node* current = list->head;
-  
-  while(current->data < val && current->next != NULL){
+  int i = 0;
+  Node* current = list->head;
+  while(i + 1 < pos){
+    if(current == NULL){
+      printf("Invalid position!\n");
+      return ;
+    }
     current = current->next;
+    i++;
   }
   
   newNode->next = current->next;
   current->next = newNode;
+  
+}
+//========================== DELETE ==============================
+void deleteHead(LinkedList* list){
+  if(list->head == NULL) return;
+  
+  Node* temp = list->head;
+  list->head = list->head->next;
+  free(temp);
+  
 }
 
-//problem: delete node as the result list is in ascending order
-//assume the given list is already in ascending order
-void deleteAscending(linkedList* list, int val){
-  if(list->head == NULL){
-    printf("List is empty!");
-    return;
-  }
+void deleteAtPos(LinkedList* list, int pos){
+  if(list->head == NULL) return;
   
-  if(list->head->data == val){
-    node* temp = list->head;
-    list->head = list->head->next;
-    free(temp);
-    return;
-  }
-  
-  node* current = list->head;
-  
-  while(current->next->data < val && current->next != NULL){
+  int i = 0;
+  Node* current = list->head;
+  while( i + 1 < pos){
+    if(current == NULL){
+      printf("Invalid position!\n");
+      return;
+    }
+    
     current = current->next;
+    i++;
   }
   
-  if(current->next != NULL && current->next->data == val){
-    node* temp = current->next;
-    current->next = temp->next;
-    free(temp);
-  } else {
-    printf("Value %d not found!\n", val);
-  }
-  
-  return;
+  Node* temp = current->next;
+  current->next = temp->next;
+  free(temp);
 }
 
 int main(){
-  int po[5] = {2, -5, 10, -12, 17};
-  linkedList list;
-  initLinkedList(&list);
+  LinkedList list;
+  initList(&list);
   
-  for(int i = 0; i < 5; i++){
-    insertAscending(&list, po[i]);
+  for(int i = 9; i >= 0; i--){
+    insertEndList(&list, i);
   }
   printList(&list);
   
-  deleteAscending(&list, 2);
+  insertHeadList(&list, 15);
+  printList(&list);
+  
+  deleteHead(&list);
+  printList(&list);
+  
+  insertAtPos(&list, 2, 10);
+  printList(&list);
+  
+  deleteAtPos(&list, 2);
   printList(&list);
   freeList(&list);
-  
- return 0; 
+  return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+List: 9 -> 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+List: 15 -> 9 -> 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+List: 9 -> 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+List: 9 -> 8 -> 10 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+List: 9 -> 8 -> 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+*/
 
 
