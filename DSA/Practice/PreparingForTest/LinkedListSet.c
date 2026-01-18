b9d49ee8fdf4baa7b909c626db07cfb508545b92
Do Quang Phong <wangphong1211@gmail.com>
2026-01-19 00:14:10 +0700

    Create LinkedListSet.c

---
+++ DSA/Practice/PreparingForTest/LinkedListSet.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node{
+  int data;
+  struct Node* next;
+}Node;
+
+typedef struct LinkedList{
+  Node* head;
+}LinkedList;
+
+void initList(LinkedList* list){
+  list->head = NULL;
+}
+
+void freeList(LinkedList* list){
+  if(list->head == NULL){
+    return;
+  }
+  
+  Node* current = list->head;
+  while(current != NULL){
+    Node* temp = current->next;
+    free(current);
+    current = temp;
+  }
+  list->head = NULL;
+}
+
+
+Node* createNode(int inVal){
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->data = inVal;
+  newNode->next = NULL;
+  return newNode;
+}
+
+void printList(LinkedList* list){
+  if(list->head == NULL){
+    printf("List is empty!\n");
+    return;
+  }
+  
+  printf("List: ");
+  Node* current = list->head;
+  while(current != NULL){
+    printf("%d -> ", current->data);
+    current = current->next;
+  }
+  printf("NULL\n");
+}
+
+//========================== INSERT ==============================
+void insertEndList(LinkedList* list, int inputVal){
+  
+  if(list->head == NULL){
+    list->head = createNode(inputVal);
+    return;
+  }
+  
+  Node* current = list->head;
+  while( current->next != NULL ){
+    current = current->next;
+  }
+  current->next = createNode(inputVal);
+  
+}
+
+void insertHeadList(LinkedList* list, int inputVal){
+  Node* newNode = createNode(inputVal);
+  
+  if(list->head == NULL){
+    list->head = newNode;
+  }
+  
+  newNode->next = list->head;
+  list->head = newNode;
+}
+
+//Assume that all the input val is valid
+void insertAtPos(LinkedList* list, int pos, int inputVal){
+  Node* newNode = createNode(inputVal);
+  
+  if(list->head == NULL){
+    list->head = newNode;
+    return;
+  }
+  
+  int i = 0;
+  Node* current = list->head;
+  while(i + 1 < pos){
+    if(current == NULL){
+      printf("Invalid position!\n");
+      return ;
+    }
+    current = current->next;
+    i++;
+  }
+  
+  newNode->next = current->next;
+  current->next = newNode;
+  
+}
+
+//========================== DELETE ==============================
+void deleteHead(LinkedList* list){
+  if(list->head == NULL) return;
+  
+  Node* temp = list->head;
+  list->head = list->head->next;
+  free(temp);
+  
+}
+
+void deleteAtPos(LinkedList* list, int pos){
+  if(list->head == NULL) return;
+  
+  int i = 0;
+  Node* current = list->head;
+  while( i + 1 < pos){
+    if(current == NULL){
+      printf("Invalid position!\n");
+      return;
+    }
+    
+    current = current->next;
+    i++;
+  }
+  
+  Node* temp = current->next;
+  current->next = temp->next;
+  free(temp);
+}
+
+int main(){
+  LinkedList list;
+  initList(&list);
+  //test case:
+  
+  for(int i = 9; i >= 0; i--){
+    insertEndList(&list, i);
+  }
+  printList(&list);
+  
+  freeList(&list);
+  return 0;
+}
