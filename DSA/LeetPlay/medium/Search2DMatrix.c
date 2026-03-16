6b5eccdc49ca11f6948a4f152449a35826e61bd3
Do Quang Phong <wangphong1211@gmail.com>
2026-03-16 13:07:29 +0700

    Implement searchMatrix function for 2D matrix

---
+++ DSA/LeetPlay/medium/Search2DMatrix.c
@@ -0,0 +1,8 @@
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    for(int i = 0; i < matrixSize; i++){
+        for(int j = 0; j < (*matrixColSize); j++){
+            if(matrix[i][j] == target) return true;
+        }
+    }
+    return false;
+}
