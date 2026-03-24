58322daafe789954ff2b3a699d49be165e6f59b2
Do Quang Phong <wangphong1211@gmail.com>
2026-03-24 21:08:43 +0700

    Implement function to reverse a linked list

---
+++ DSA/LeetPlay/easy/ReverseLinkedList.c
@@ -0,0 +1,14 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    
+    while (curr != NULL) {
+        struct ListNode* next = curr->next; 
+        curr->next = prev;                 
+        
+        prev = curr;                       
+        curr = next;                       
+    }
+    
+    return prev; 
+}
