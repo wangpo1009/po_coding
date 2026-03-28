8decddfd6599a0356fa472ad5a1156f427a134bd
Do Quang Phong <wangphong1211@gmail.com>
2026-03-28 12:57:37 +0700

    Create classHouse.c

---
+++ OOP/Week 2/classHouse.c
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class House{
+  private:
+  string address;
+  int theNumberOfRoom;
+  
+  public:
+  House(string address, int theNumberOfRoom){
+    this->setAddress(address);
+    this->setTheNumberOfRoom(theNumberOfRoom);
+  }
+  
+  void setTheNumberOfRoom(int theNumberOfRoom){
+    this->theNumberOfRoom = theNumberOfRoom;
+  }
+  
+  void setAddress(string address){
+    this->address = address;
+  }
+  
+  string getAddress(){
+    return address;
+  }
+  
+  int getTheNumberOfRoom(){
+    return theNumberOfRoom;
+  }
+  
+  void extendRoom(int amountOfRoom){
+    if(-amountOfRoom > this->getTheNumberOfRoom() && amountOfRoom < 0){
+      cout << "INVALID AMOUNT OF ROOM!" << "\n";
+      return;
+    }
+    
+    this->setTheNumberOfRoom(this->getTheNumberOfRoom() + amountOfRoom);
+    cout << "SUCCESS!" << "\n";
+  }
+  
+};
+  
+  
+  
