3fb3cd935fda95e0af60b7ff3a775b51dc53cefc
Do Quang Phong <wangphong1211@gmail.com>
2026-05-05 16:37:19 +0700

    Implement Employee, Programmer, and SoftwareHouse classes
    Defined classes for Employee, Programmer, and SoftwareHouse with appropriate constructors, setters, and getters.

---
+++ OOP/Week 4/defineThings.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Employee;
+class SoftwareHouse;
+class Programmer;
+
+class Employee{
+  private:
+  string number;
+  double salary;
+  string name;
+  
+  public:
+  Employee(string number, double salary, string name){
+    this->setNumber(number);
+    this->setSalary(salary);
+    this->setName(name);
+  }
+  Employee() : Employee("000", 0, " "){}
+  
+  //===== Setter =====
+  void setName(string name){
+    this->name = name;
+  }
+  void setNumber(string number){
+    this->number = number;
+  }
+  void setSalary(double salary){
+    this->salary = salary;
+  }
+  
+  //===== Getter =====
+  string getName(){
+    return name;
+  }
+  double getSalary(){
+    return salary;
+  }
+  string getNumber(){
+    return number;
+  }
+  
+  
+};
+
+class Programmer : public Employee{
+  private:
+  string language;
+  
+  public:
+  Programmer(string number, double salary, string name, string language) : Employee(number, salary, name){
+    this->setLanguage(language);
+  }
+  Programmer() : Programmer("000", 0, " ", "cpp"){}
+  
+  void setLanguage(string language){
+    this->language = language;
+  }
+  
+  string getLanguage(){
+    return language;
+  }
+};
+
+class SoftwareHouse{
+  private:
+  string name;
+  vector <Programmer> programmerList;
+  
+  public:
+  SoftwareHouse(string name){
+    this->setName(name);
+  }
+  SoftwareHouse() : SoftwareHouse("May Thg Kho"){}
+  
+  void setName(string name){
+    this->name = name;
+  }
+  
+  string getName(){
+    return name;
+  }
+  
+  void addProgrammer(Programmer A){
+    programmerList.push_back(A);
+  }
+};
+
+int main(){
+  cout << "No hope";
+  return 0;
+}
